5c1597257d18691d8e52327b797e2baf34b4c4c9
Yi Xin Tan <75169586+YiXin-Tan@users.noreply.github.com>
2024-04-08 15:55:19 +1000

    added url checker files

--- constants.py
+++ constants.py
@@ -1,4 +1,4 @@
 PROJECT_ID = 'metal-node-415712'
-PROCESSOR_ID = ''
+PROCESSOR_ID = 'ba498bb1edd15961'
 DJANGO_SECRET_KEY = 'django-insecure-b-*@#eve6m5+014qri1(!uo+#tam^w4$p+6bsorc_qioo5@yw_'
-YX_UP_PAT = ''
+YX_UP_PAT = 'up:yeah:wYw1eZLAjH86FXm4ahrFChXHX5jEJ97Qv8hqr24AsIWoirDoTerv4P0e0UMNBBpYHspT77tqAEOI08GNigEX0ns4NL8vzx5f8GGuw4t7SbycDKrCR6JNN57dampyyutX'
